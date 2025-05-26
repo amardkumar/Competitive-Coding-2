19341b0b8d883560342e4522a45c61a70edb22d6
amardkumar <102793539+amardkumar@users.noreply.github.com>
2025-05-26 12:35:01 -0400

    Two Sum Implementation

---
+++ Problem2.java
@@ -0,0 +1,20 @@
+//Time Complexity : O(n)
+//Space Complexity : O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        if(nums.length == 0 ) return null;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i< nums.length;i++){
+            int delta = target - nums[i];
+
+            if(map.containsKey(delta)){
+                return new int []{map.get(delta), i};
+            }
+            map.put(nums[i], i);
+        }   
+        return null;
+    }
+}
